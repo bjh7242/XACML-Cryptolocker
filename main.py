6b9be429ccfd1b81921ba606ba6e3e2d801c78b3
bjh7242 <bjh7242@rit.edu>
2015-11-22 20:22:28 -0500

    Edited argparsing options. Added get_file_list()

--- main.py
+++ main.py
@@ -19,21 +19,48 @@ def decrypt(files):
 	"""
 	pass
 
+def get_file_list(directory, recurse):
+	"""
+	This function returns a list of files based off of the given directory and 
+	will recurse, if desired
+	"""
+	files = []
+	
+	if recurse == False:
+		print "Recurse false"
+
+	elif recurse == True:
+		print "Recurse true"
+
+	else:
+		print "Something weird happened."
+		sys.exit(1)
+
+
+	return files
 
 def main():
 	parser = argparse.ArgumentParser(description='Encrypt and decrypt specified files')
 	parser.add_argument('-D', help='Directory to start encrypting files in', dest="directory", required="True")
-	parser.add_argument('-R', help='Specify whether to recurse down directories', dest="recurse", default=False)
+	parser.add_argument('-R', help='Specify whether to recurse down directories', dest="recurse", action="store_true", default=False)
 	parser.add_argument('-a', help='Action to perform [encrypt/decrypt]', dest="action", required="True")
 
 	args = parser.parse_args()
 
-	# if args.action != 'encrypt' and args.action != 'decrypt', throw an error
-	if args.action != "encrypt" and args.action != "decrypt":
+	# if args.action is neither 'encrypt' nor 'decrypt', throw an error
+	if args.action == "encrypt":
+		print "Encrypting files..."
+		encrypt(get_file_list(args.directory, args.recurse))
+
+	elif args.action == "decrypt":
+		print "Decrypting files..."
+		decrypt(get_file_list(args.directory, args.recurse))
+
+	# else, if neither argument is specified, throw an error
+	else:
 		print "ERROR: -a must specify either 'encrypt' or 'decrypt' as an argument"
 		sys.exit(1)
 
-	
 	return 0
 
 
