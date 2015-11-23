166fd70972ff86e8efbd1602831e044726551a1a
bjh7242 <bjh7242@rit.edu>
2015-11-22 19:53:02 -0500

    Added argparse functionality

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ def decrypt(files):
 
 
 def main():
-	pass
+	parser = argparse.ArgumentParser(description='Encrypt and decrypt specified files')
+	parser.add_argument('-D', help='Directory to start encrypting files in', dest="directory", required="True")
+	parser.add_argument('-R', help='Specify whether to recurse down directories', dest="recurse", default=False)
+	parser.add_argument('-a', help='Action to perform [encrypt/decrypt]', dest="action", required="True")
+
+	args = parser.parse_args()
+
+	# if args.action != 'encrypt' and args.action != 'decrypt', throw an error
 
 
 if __name__ == '__main__':
