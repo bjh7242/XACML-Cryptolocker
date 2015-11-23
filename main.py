a07bc6932580d7826722451898478a82b8b98fab
bjh7242 <bjh7242@rit.edu>
2015-11-22 19:32:08 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+
+
+def encrypt(files):
+	"""
+	This function encrypts the specified file using RC4
+	Args: list of files to encrypt
+	"""
+	pass
+
+def decrypt(files):
+	"""
+	This function decrypts the specified file that was encrypted using RC4
+	Args: list of files to decrypt
+	"""
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	sys.exit(main())
